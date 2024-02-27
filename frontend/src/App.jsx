5bfca80df47c72434dba3d76b889a404112df6c0
Stiven-Yepes-Arenas <estivenyepesarenas@gmail.com>
2024-02-27 18:53:19 -0500

    Aagregando estilos basicos de chat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import io from "socket.io-client";
 import { useState, useEffect } from "react";
+
 const socket = io("/");
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
       from: "Me",
     };
     setMensajes([...mensajes, newMensaje]);
-    socket.emit("mensaje", mensaje); //Desde este evento enviamos lo que escriben en el formulario a nuestro back.
+    socket.emit("mensaje", mensaje);
     setMensaje("");
   };
 
@@ -30,23 +31,41 @@ function App() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="h-screen flex flex-col justify-between bg-gray-900 text-white  ">
+      <div className="flex-grow flex flex-col justify-end">
+        <ul className="flex flex-col items-start gap-2">
+          {mensajes.map((mensaje, i) => (
+            <li
+              key={i}
+              className={`my-2 p-2 table text-sm rounded-md ${
+                mensaje.from === "Me"
+                  ? "bg-blue-500 self-end justify-self-end"
+                  : "bg-green-500 self-start"
+              }`}
+            >
+              <span className="text-xs text-slate-300 block">
+                {mensaje.from}
+              </span>
+              <span className="text-md">{mensaje.body}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <form onSubmit={handleSubmit} className="flex justify-between px-4 py-2">
         <input
           type="text"
           placeholder="Escribe tu Mensaje"
           value={mensaje}
           onChange={(e) => setMensaje(e.target.value)}
+          className="flex-grow px-4 py-2 rounded-md bg-gray-800 text-white"
         />
-        <button type="submit">Enviar</button>
+        <button
+          type="submit"
+          className="ml-2 px-4 py-2 bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+        >
+          Enviar
+        </button>
       </form>
-      <ul>
-        {mensajes.map((mensaje, i) => (
-          <li key={i}>
-            {mensaje.from} : {mensaje.body}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
